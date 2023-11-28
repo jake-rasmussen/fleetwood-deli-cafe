eec32708ee2cdc2195b8acac5e376f07423e572c
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2023-11-28 14:05:38 -0500

    make images priority

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -47,6 +47,7 @@ const Carousel = (props: PropType) => {
           height="0"
           sizes="100vw"
           className="w-full h-full object-cover"
+          priority={true}
         />
       </div>
     )});
